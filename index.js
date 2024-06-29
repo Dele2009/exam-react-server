a0e4d4b376809ccfff8ad8d26648df4044c5819b
Bamidele Aminu <obedaminu303@gmail.com>
2024-06-29 12:20:34 +0100

    ----

--- index.js
+++ index.js
@@ -13,9 +13,10 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
+const mu = process.env.MONGO_URI
+// const mu = 'mongodb://localhost:27017/exam-system'
 
-
-mongoose.connect('mongodb://localhost:27017/exam-system').then(()=>{
+mongoose.connect(mu).then(()=>{
     console.log('db connected')
 });
 
