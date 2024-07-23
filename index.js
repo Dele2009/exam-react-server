059d8fe23ed859298a7aed910d5be702010a4e1a
Bamidele Aminu <obedaminu303@gmail.com>
2024-07-23 21:01:17 +0100

    get exams api update

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 
-// const mu = process.env.MONGO_URI
-const mu = 'mongodb://localhost:27017/exam-system'
+const mu = process.env.MONGO_URI
+// const mu = 'mongodb://localhost:27017/exam-system'
 
 mongoose.connect(mu).then(()=>{
     console.log('db connected')
